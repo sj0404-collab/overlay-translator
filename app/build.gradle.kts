701795549e39597e064a9d31f2bed972abc21ac1
sj0404-collab <sj0404-collab@users.noreply.github.com>
2026-08-13 16:35:21 +0000

    Fix Tesseract: JitPack Tesseract4Android

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,5 +41,5 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("com.microsoft.onnxruntime:onnxruntime-android:1.19.2")
-    implementation("cz.adaptech.tesseract4android:tesseract4android:4.7.0")
+    implementation("com.github.adaptech-cz:Tesseract4Android:4.8.0")
 }
